c74df17caffeeb10cc1d7b689e960d97981ab9f5
hagaryota <ryota_haga@mediado.jp>
2022-04-12 14:20:43 +0900

    dependent destoryを追加、foodとorderのアソシエーション修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,4 @@
 class Shop < ApplicationRecord
-    has_many :foods
-    has_many :addresses, as: :addressable
+    has_many :foods, dependent: :destroy
+    has_many :addresses, as: :addressable, dependent: :destroy
 end
